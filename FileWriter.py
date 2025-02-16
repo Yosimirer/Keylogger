a7c9f514f66b900312c5d40204462c20a2201b3a
moti7846 <8563816@gmail.com>
2025-02-16 14:24:57 +0200

    FileWriter in class 0.3

--- FileWriter.py
+++ FileWriter.py
@@ -10,7 +10,10 @@ class FileWrite(IWriter):
     def __init__(self):
         self.keys = []
 
+
+
     def send_data(self, data, machine_name):
+        machine_name = machine_name .replace(":","_")
         with open(f"{machine_name}.json", "w") as file:
             for key in data:
                 self.keys.append(key)

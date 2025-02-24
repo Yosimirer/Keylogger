1c720218280a0d58908eec68de5116ab407f394f
yosi_mirer <yos.mirer@gmail.com>
2025-02-24 16:34:34 +0200

    java script add remove and update computers

--- FrontEnd/javaScript.js
+++ FrontEnd/javaScript.js
@@ -1,20 +1,23 @@
-document.addEventListener('DOMContentLoaded',(event) => {
+document.addEventListener('DOMContentLoaded',() => {
     document.getElementById("login-form").addEventListener('submit',(event) => {
         event.preventDefault();
-    if (this.username.value === "admin" && this.password.value === '1234') {
-        document.getElementById('login-form').style.display = 'none';
-        document.getElementById('deshboard=page').style.display = 'block';
-        showPage("dashboard");
-    } else {
-        alert('שם משתמש או הסיסמה שגויים');
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        if (username === "admin" && password === '1234') {
+            document.getElementById('login-form').style.display = 'none';
+            document.getElementById('deshboard=page').style.display = 'block';
+            showPage("dashboard");
+        } else {
+            alert('שם משתמש או הסיסמה שגויים');
     }
 
     });
 });
-
+// רשימת מחשבים
 let computers = []
 let monitoring = []
-
+// הוספת מחשב
 function addComputer(event) {
     event.preventDefault();
     const form = event.target;
@@ -22,13 +25,43 @@ function addComputer(event) {
         id: Date.mow(),
         name: form.computerName.value,
         ip: form.computerIp.value,
-        status: pass,
-        lastActiviti: new Date().toLocaleString()
-
-
+        status: "offline",
+        lastActivity: new Date().toLocaleString()
     };
+
     computers.push(computer)
     form.reset()
+    updateComputersList()
 
 
 }
+// הסרת מחשב
+function removeComputer(id){
+    computers = computers.filter(computer => computer.id !== id)
+    updateComputersList()
+}
+
+// עדכון רשימת מחשבים
+function updateComputersList() {
+    const cbody = document.getElementById('computers-list')
+    cbody.innerHTML = ''
+
+    computers.forEach(computer => {
+        const tr = document.createElement('tr')
+        tr.innerHTML = 
+        `<td>${computer.name}</td>  
+        <td>${computer.ip}</td> 
+        <td>${computer.status}</td> 
+        <td>${computer.lastActivity}</td>
+        <td>
+        <button onclick="removeComputer(${computer.id})">מחק</button>
+        <button onclick="showMonitoring(${computer.id})">מעקב</button>
+        </td>`
+    ;
+        cbody.appendChild(tr)   
+        
+    });
+
+}    
+
+

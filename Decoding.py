38086c4d22b05299b7cbb76a66900c28d41c000e
yosi_mirer <yos.mirer@gmail.com>
2025-02-27 12:43:59 +0200

    fixed Encription

--- Decoding.py
+++ Decoding.py
@@ -5,9 +5,18 @@ class Decoding:
     def reverse_ascii_xor(self, data, key="F"):
         decoded_data = ""
         key_value = ord(key)
-        for char in data:
-            char_code = ord(char)
-            decoded_char = chr(char_code ^ key_value)
-            decoded_data += decoded_char
-        return decoded_data
 
+        if isinstance(data, dict):
+            result = {}
+            for timestamp, encrypted_chars in data.items():
+                decoded_chars = []
+                for encrypted_char in encrypted_chars:
+                    decoded_char = chr(ord(encrypted_char) ^ key_value)
+                    decoded_chars.append(decoded_char)
+                result[timestamp] = ''.join(decoded_chars)
+            return result
+        else:
+            for char in data:
+                decoded_char = chr(ord(char) ^ key_value)
+                decoded_data += decoded_char
+            return decoded_data
